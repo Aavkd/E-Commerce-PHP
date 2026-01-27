6d3aba3bb8c0c0d44bcec0cf651ca0414462c6dc
Alexy <alexy.traore@ynov.com>
2026-01-27 17:22:34 +0100

    fixed logout redirect

--- public/api/logout.php
+++ public/api/logout.php
@@ -1,8 +1,9 @@
 <?php
 // api/logout.php
-header('Content-Type: application/json');
+
 require_once '../../src/Utils/Auth.php';
 
 Auth::logout();
-echo json_encode(['success' => true, 'message' => 'Déconnecté']);
+header('Location: /login.php');
+exit;
 ?>
